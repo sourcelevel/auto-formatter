199dddc4a5b92aac132bdc8b943202405af83022
Beatriz Chagas <beatriznery.12@gmail.com>
2022-02-04 14:51:35 -0300

    Generate app using `mix new` command
    Co-authored-by: wevtimoteo <wevtimoteo@users.noreply.github.com>
    Co-authored-by: garaujodev <garaujodev@users.noreply.github.com>

---
+++ lib/auto_formatter.ex
@@ -0,0 +1,18 @@
+defmodule AutoFormatter do
+  @moduledoc """
+  Documentation for `AutoFormatter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AutoFormatter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/auto_formatter_test.exs
@@ -0,0 +1,8 @@
+defmodule AutoFormatterTest do
+  use ExUnit.Case
+  doctest AutoFormatter
+
+  test "greets the world" do
+    assert AutoFormatter.hello() == :world
+  end
+end
