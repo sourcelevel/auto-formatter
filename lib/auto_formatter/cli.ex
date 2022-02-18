5c6f3275be035d811313a54523479b7c5b6d47ea
Gustavo Araujo <garaujodev@gmail.com>
2022-02-18 15:36:56 -0300

    Parse args to call `AutoFormatter.Action` module
    Co-authored-by: wevtimoteo <wevtimoteo@users.noreply.github.com>
    Co-authored-by: beatrizChagas <beatrizChagas@users.noreply.github.com>

--- lib/auto_formatter/cli.ex
+++ lib/auto_formatter/cli.ex
@@ -4,6 +4,20 @@ defmodule AutoFormatter.CLI do
   @actions ~w[init enable disable version]
 
   def main(args \\ []) do
-    IO.inspect(args)
+    args
+    |> parse_args()
+    |> execute()
+    |> output()
   end
+
+  defp parse_args([action]) when action in @actions, do: String.to_atom(action)
+  defp parse_args(_args), do: :help
+
+  defp execute(:help) do
+    "Usage: autoformatter [action] [options]"
+  end
+
+  defp execute(action), do: Action.perform(action)
+
+  defp output(message), do: IO.puts(message)
 end
