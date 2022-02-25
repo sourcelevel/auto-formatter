38859fa204ca8d1988d4f79dfba15cf64d419539
Beatriz Chagas <beatriznery.12@gmail.com>
2022-02-25 15:22:56 -0300

    Implement init action
    Co-authored-by: wevtimoteo <wevtimoteo@users.noreply.github.com>
    Co-authored-by: garaujodev <garaujodev@users.noreply.github.com>

--- lib/auto_formatter/action.ex
+++ lib/auto_formatter/action.ex
@@ -1,6 +1,24 @@
 defmodule AutoFormatter.Action do
+  @pre_commit_hook_content """
+    #!/bin/sh
+    set -e
+    # Hook from autoformatter 🔎
+    autoformatter _format
+  """
+
+  @pre_commit_hook_path "./.git/hooks/pre-commit"
+
   def perform(:version) do
     AutoFormatter.version()
+  end
+
+  def perform(:init) do
+    case File.write(@pre_commit_hook_path, @pre_commit_hook_content) do
+      :ok ->
+        File.chmod(@pre_commit_hook_path, 0o755)
+      _ ->
+        :error
+    end
   end
 
   def perform(action) do
--- test/auto_formatter_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AutoFormatterTest do
-  use ExUnit.Case
-  doctest AutoFormatter
-
-  test "greets the world" do
-    assert AutoFormatter.hello() == :world
-  end
-end
