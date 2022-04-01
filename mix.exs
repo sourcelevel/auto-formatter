5993d9fa9b039e0404a28cfab2e85f3c989c4542
Gustavo Araujo <garaujodev@gmail.com>
2022-04-01 15:26:52 -0300

    Update `mix.exs` to publish the first release
    Co-authored-by: wevtimoteo <wevtimoteo@users.noreply.github.com>
    Co-authored-by: beatrizChagas <beatrizChagas@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -4,11 +4,18 @@ defmodule AutoFormatter.MixProject do
   def project do
     [
       app: :auto_formatter,
-      version: "0.1.0",
+      description: "Mix escript to run mix format on precommit git hook",
+      version: "0.1.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      escript: escript()
+      package: package(),
+      escript: escript(),
+      # Docs
+      name: "AutoFormatter",
+      source_url: "https://github.com/sourcelevel/auto-formatter",
+      homepage_url: "https://hex.pm/packages/auto_formatter/",
+      docs: docs()
     ]
   end
 
@@ -22,15 +29,32 @@ defmodule AutoFormatter.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
+  defp package do
+    [
+      name: "auto_formatter",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sourcelevel/auto-formatter"}
+    ]
+  end
+
   defp escript do
     [
+      name: "autoformatter",
       main_module: AutoFormatter.CLI,
-      path: "_build/autoformatter"
+      path: "autoformatter"
+    ]
+  end
+
+  defp docs do
+    [
+      main: "AutoFormatter",
+      extras: ~w(README.md)
     ]
   end
 end
