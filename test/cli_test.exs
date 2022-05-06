39345eae04b3aecd4e03fef2f2dc02e17f8e6eb4
Gustavo Araujo <garaujodev@gmail.com>
2022-05-06 15:20:14 -0300

    Add test scenario for `CLI` module
    Co-authored-by: wevtimoteo <wevtimoteo@users.noreply.github.com>
    Co-authored-by: beatrizChagas <beatrizChagas@users.noreply.github.com>

---
+++ test/cli_test.exs
@@ -0,0 +1,11 @@
+defmodule AutoFormatter.CLITest do
+  use ExUnit.Case, async: true
+
+  import ExUnit.CaptureIO
+
+  test ".main/1 when receives help prints the usage instructions" do
+    assert capture_io(fn ->
+      AutoFormatter.CLI.main(["help"])
+    end) == "Usage: autoformatter [action] [options]\n"
+  end
+end
